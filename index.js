bde8763238b697a21c827aab6ae5805d023b1133
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-07-27 03:24:52 -0700

    added task-1 and task-2

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const testOperation = (num1,num2, callbackFunction) => {
-    const result = callbackFunction(num1, num2);
-     console.log("Result:", result);
-  };
-  
-  const addNumbers = (a, b) => a + b;
-  
-  const multiplyNumbers = (x, y) => x * y;
-  
-  testOperation(10, 5, addNumbers); 
-  testOperation(7, 3, multiplyNumbers); 
+
+// Task-1
+const reversString=(inputString)=>{
+    let reversString="";
+    for(let i= inputString.length -1; i >= 0; i--){
+        reversString += inputString[i];
+    }
+    return reversString
+}
+
+const inputStr="hello world";
+const reversedString= reversString(inputStr);
+console.log(reversedString)
+
+// task-2
+const allPositiveNumbers=(numbers)=>{
+     let sum=0;
+     for(const num of numbers){
+        if(num >0){
+            sum += num;
+        }
+     }
+     return sum;
+}
+
+const numbersArr=[2, -5, 10, -3, 7];
+const result= allPositiveNumbers(numbersArr)
+console.log(result)
